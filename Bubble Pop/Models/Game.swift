1356f9bba6f895886581fa9436f6f3ca58c38068
Kowstof <krystofpavlis@gmail.com>
2021-04-24 23:49:04 +1000

    Added basic MVC components

---
+++ Bubble Pop/Models/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  Bubble Pop
+//
+//  Created by Krystof Pavlis on 24/4/21.
+//
+
+import Foundation
+
---
+++ Bubble Pop/Models/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  Bubble Pop
+//
+//  Created by Krystof Pavlis on 24/4/21.
+//
+
+import Foundation
